0a4cdec4cb094a2777988eeb14d6e6ca8ffea1ef
kart11ik <karthikmarar.11@gmail.com>
2023-03-29 12:38:34 +0530

    y

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
-
 from .models import Tasks
 
 
@@ -78,51 +77,62 @@ class TaskDetailView(DetailView):
 #     return render(request, "login.html", context)
 
 
-#def register_fun(request):
-    # register = register.objects.all()
-    # form = RegisterForm()
-    # if request.method == 'POST':
-    #     form = RegisterForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #     return redirect('/task/')
-    # context = {'register': register, 'form': form}
-    # return render(request, 'register.html', context)
+# def register_fun(request):
+# register = register.objects.all()
+# form = RegisterForm()
+# if request.method == 'POST':
+#     form = RegisterForm(request.POST)
+#     if form.is_valid():
+#         form.save()
+#     return redirect('/task/')
+# context = {'register': register, 'form': form}
+# return render(request, 'register.html', context)
 
-    # context = {}
-    # s = RegisterForm(request.POST)
-    # if s.is_valid():
-    #     s.save()
-    #     return redirect('/task')
-    # context['form'] = s
-    # return render(request, "register.html", context)
+# context = {}
+# s = RegisterForm(request.POST)
+# if s.is_valid():
+#     s.save()
+#     return redirect('/task')
+# context['form'] = s
+# return render(request, "register.html", context)
 def signup(request):
-
     if request.method == 'POST':
         username = request.POST['username']
-        fname=request.POST['fname']
+        fname = request.POST['fname']
         lname = request.POST['lname']
-        email=request.POST['email']
-        pass1=request.POST['pass1']
-        pass2=request.POST['pass2']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
 
-        myuser=User.objects.create_user(username,email,pass1)
-        myuser.first_name=fname
-        myuser.last_name=lname
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
 
         myuser.save()
-        messages.success(request,"You created an account!")
+        messages.success(request, "You created an account!")
 
         return redirect('login')
 
-    return render(request,'register.html')
+    return render(request, 'register.html')
 
 
 class CustomLogin(LoginView):
-    template_name='login.html'
-    fields ='_all_'
+    template_name = 'login.html'
+    fields = '_all_'
     success_url = reverse_lazy('task')
-    redirected_authenticated_user=True
+    redirected_authenticated_user = True
 
     def get_success_url(self):
         return self.success_url
+
+
+# def mail(request):
+#     subject = "Greetings"
+#     msg = "Congratulations for ur success"
+#     to = "abc@gamil.com"
+#     res = send_mail(subject,msg,settings.EMAIL_HOST_USER,[to])
+#     if res==1:
+#         msg= "mail send successfully"
+#     else:
+#         msg= "could not send email"
+#     return HttpResponse(msg)
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -101,6 +101,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
+
+
+
+
+
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
